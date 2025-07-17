69b4c5ab8a692dad8b7f4428e7e0acd914cb1f88
RomanBorabo <fedorenkoroman@bk.ru>
2025-07-17 09:15:03 +0300

    Delete statusTranslation.js

--- statusTranslation.js
+++
@@ -1,9 +0,0 @@
-function translateStatus(status) {
-  const map = {
-    "STATUS_PAYMENT_PENDING": "Ожидает оплаты",
-    "STATUS_PAID": "Оплачено",
-    "STATUS_CANCELLED": "Отменено",
-    "STATUS_EXPIRED": "Истек срок оплаты",
-  };
-  return map[status] || status || "Неизвестно";
-}
